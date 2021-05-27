ab488b16c1d157fa82231f27bb4b715f446386ad
gmy1994 <mengyao_guo@whu.edu.cn>
2021-05-27 13:59:06 -0500

    add the relative difference between OAK and MAPLE

---
+++ clim_monthly_dif.py
@@ -0,0 +1,29 @@
+# Calculate the ralative difference (%) of monthly climatology for Oak (10m) vs. Maple
+import os
+import os.path
+import subprocess
+import numpy as np
+import netCDF4 as nc
+
+INDIR = "/scratch/06956/mguo/NoahMP_PHS/CN/experiment/clim_monthly/"
+TEMPLATE = "./clim_monthly_TEM.cdl"
+OUTDIR = "/scratch/06956/mguo/NoahMP_PHS/CN/experiment/clim_monthly/dif/"
+
+os.system("mkdir " + OUTDIR)
+
+month = ['01','02','03','04','05','06','07','08','09','10','11','12']
+
+for m in range(len(month)):
+    os.system("mkdir " + OUTDIR + month[m] + "/")
+
+    oak = INDIR + "oak_10m/" + month[m] + "/" + "clim.nc"
+    map = INDIR + "map_2m/" + month[m] + "/" + "clim.nc"
+
+    vars = ["clim_ET","clim_TR","clim_TR_ET","clim_VBTRAN","clim_SFC_RO","clim_UGD_RO","clim_RO","clim_SM"]
+
+    output = OUTDIR + month[m] + "/" + "oak_vs_map.nc"
+    os.system("rm " + output)
+    subprocess.run(['ncgen', '-3', '-o', output, TEMPLATE])
+    with nc.Dataset(output, 'a') as ncf:
+        for var in vars:
+            ncf.variables[var][:] = 100 * (nc.Dataset(oak).variables[var][:] - nc.Dataset(map).variables[var][:]) / nc.Dataset(oak).variables[var][:]
